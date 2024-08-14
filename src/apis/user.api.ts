90419b78a66d3c91c5ac8ffc6fc9ad8189ce6a97
ManhDev24 <tienmanhpham2403@gmail.com>
2024-08-15 00:25:43 +0700

    update list user

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -1,5 +1,5 @@
 import { Welcome } from '../interface'
-import { CurrentUser, UserLoginRequest, UserRegisterRequest } from '../interface/user.interface'
+import { CurrentUser, EditUser, UserLoginRequest, UserRegisterRequest } from '../interface/user.interface'
 import fetcher from './fetcher'
 
 export const userAPI = {
@@ -14,6 +14,38 @@ export const userAPI = {
   register: async (data: UserRegisterRequest) => {
     try {
       const response = await fetcher.post('/Users/signup', data)
+      return response.data.content
+    } catch (error: any) {
+      throw Error(error.response.data.message)
+    }
+  },
+  getListUser: async () => {
+    try {
+      const response = await fetcher.get('/Users/getUser')
+      return response.data.content
+    } catch (error: any) {
+      throw Error(error.response.data.message)
+    }
+  },
+  getDetailuserById: async (id: string) => {
+    try {
+      const response = await fetcher.get(`/Users/getUser?keyword=${id}`)
+      return response.data.content
+    } catch (error: any) {
+      throw Error(error.response.data.message)
+    }
+  },
+  editUser: async (payload: EditUser) => {
+    try {
+      const response = await fetcher.put('/Users/editUser', payload)
+      return response.data.content
+    } catch (error: any) {
+      throw Error(error.response.data.message)
+    }
+  },
+  deleteUser: async (id: string) => {
+    try {
+      const response = await fetcher.delete(`/Users/deleteUser?id=${id}`)
       return response.data.content
     } catch (error: any) {
       throw Error(error.response.data.message)
---
+++ src/layout/Users/index.ts
@@ -0,0 +1,1 @@
+export { default as userShow } from './User'
