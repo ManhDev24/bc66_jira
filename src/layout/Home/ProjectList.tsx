d125d2a94af5719bd22dea30dcc99c793333c26e
Thanh Tan <nhanthanhtandev@gmail.com>
2024-10-10 16:06:11 +0700

    sua chua thong bao khi edit

--- src/layout/Home/ProjectList.tsx
+++ src/layout/Home/ProjectList.tsx
@@ -21,6 +21,7 @@ import { useListProject } from "../../hooks/useListProject";
 import { Project } from "../../interface/projectListInter";
 import { projectApi } from "../../apis/projects.api";
 import {
+  
   deleteProjectApi,
   getAllProject,
   getAllProjectCategoryAction,
@@ -36,6 +37,7 @@ import { toast } from "react-toastify";
 interface Project {
   id: number
   name: string
+  creator:string
   // Thêm các thuộc tính khác
 }
 
@@ -50,9 +52,13 @@ const ProjectList: React.FC = () => {
   // const { projectList } = useSelector((state: any) => state.projectReducer)
   const [totalItems, setTotalItems] = useState(0)
   const user = useAppSelector((state:any) => state.user.currentUser)
+  // const { projectDetail} = useSelector(
+  //   (state:any) => state.projectReducer
+  // );
+  // console.log(projectDetail)
   const projectRef = useRef([])
   const searchRef = useRef(null)
-
+  
   const suffix = (
     <AudioOutlined
       style={{
@@ -182,7 +188,14 @@ const ProjectList: React.FC = () => {
               type="primary"
               className="mr-2"
               onClick={() => {
-                navigate(`/projects/${record.id}/edit`)
+                // console.log(record.creator.name);
+
+                if(record.creator.name === user.name){
+                  navigate(`/projects/${record.id}/edit`)
+                }
+                else{
+                  toast("Bạn không phải người tạo dự án nên không thể edit");
+                }
               }}
             >
               Setting
@@ -308,7 +321,12 @@ const ProjectList: React.FC = () => {
                       type="primary"
                       className="mr-2"
                       onClick={() => {
-                        navigate(`/projects/${projectIdData}/edit`);
+                        if(projectDetail.creator.name === user.name){
+                          navigate(`/projects/${projectIdData}/edit`);
+                        }
+                        else{
+                          toast("Bạn không phải người tạo nên thông thể edit");
+                        }
                       }}
                     >
                      <span role="img" aria-label="edit" className="anticon anticon-edit"><svg viewBox="64 64 896 896" focusable="false" data-icon="edit" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M257.7 752c2 0 4-.2 6-.5L431.9 722c2-.4 3.9-1.3 5.3-2.8l423.9-423.9a9.96 9.96 0 000-14.1L694.9 114.9c-1.9-1.9-4.4-2.9-7.1-2.9s-5.2 1-7.1 2.9L256.8 538.8c-1.5 1.5-2.4 3.3-2.8 5.3l-29.5 168.2a33.5 33.5 0 009.4 29.8c6.6 6.4 14.9 9.9 23.8 9.9zm67.4-174.4L687.8 215l73.3 73.3-362.7 362.6-88.9 15.7 15.6-89zM880 836H144c-17.7 0-32 14.3-32 32v36c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-36c0-17.7-14.3-32-32-32z"></path></svg></span>
